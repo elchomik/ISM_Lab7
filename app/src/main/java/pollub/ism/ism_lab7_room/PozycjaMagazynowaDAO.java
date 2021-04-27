3c5dad3e36d608fc9782da6d9a2b3024b1026666
Mateusz M <mateuszmarkowski101@wp.pl>
2021-04-27 13:19:40 +0200

    Add Files to Lab7

---
+++ app/src/main/java/pollub/ism/ism_lab7_room/PozycjaMagazynowaDAO.java
@@ -0,0 +1,26 @@
+package pollub.ism.ism_lab7_room;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+@Dao
+public interface PozycjaMagazynowaDAO {
+
+    @Insert
+    public void insert(PozycjaMagazynowa pozycjaMagazynowa);
+
+    @Update
+    void update(PozycjaMagazynowa pozycjaMagazynowa);
+
+    @Query("SELECT QUANTITY FROM Warzywniak WHERE NAME= :wybraneWarzywoNazwa")
+    int findQuantityByName(String wybraneWarzywoNazwa);
+
+    @Query("UPDATE Warzywniak SET QUANTITY =:wybraneWarzywoNowaIlosc WHERE NAME= :wybraneWarzywoNazwa")
+    void updateQuantityByName(String wybraneWarzywoNazwa, int wybraneWarzywoNowaIlosc);
+
+    @Query("SELECT COUNT(*) FROM Warzywniak")
+    int size();
+
+}
